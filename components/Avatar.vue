4e17e04e9ad6f7fc99d9996e1b314a7405ad41a6
Csaba Makara <csaba.makara@gmail.com>
2018-10-27 23:01:58 +0200

    feat(avatar): added new component

---
+++ components/Avatar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="avatar">
+    <nuxt-link to="/">
+      <img class="avatar-image" src="https://i.pinimg.com/originals/c8/e7/e6/c8e7e6bed6fc459ff62ac1f11abf7987.jpg">
+    </nuxt-link>
+  </div>
+</template>
+
+<style>
+.avatar {
+  padding: 1rem 0 1rem 0;
+  background-color: #3879a2;
+}
+
+.avatar-image {
+  height: 3rem;
+  width: 3rem;
+  margin-top: 0.5 rem;
+}
+.uk-divider-small {
+  margin: 1em 0 1em 0;
+}
+</style>
+
