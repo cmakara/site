9a894c9c14b772c9def3dabdbd4838fcdf8d9bd4
Csaba Makara <csaba.makara@gmail.com>
2018-10-27 23:02:38 +0200

    feat(menu-items): added menu item components

---
+++ components/MenuItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="['menu-item', expanded ? 'expanded' : '' ]">
+    <div class="uk-flex uk-flex-row uk-flex-center">
+      <div class="nav-icon uk-flex uk-flex-center" :uk-icon="`icon: ${icon}`" title="About me" />
+      <div class="nav-text" v-if="expanded">
+        {{text}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["expanded", "text", "icon"]
+};
+</script>
+
+
+<style>
+.menu-item {
+  width: 4rem;
+  align-items: center;
+  text-align: center;
+  padding: 1rem 0 1rem 0;
+}
+.menu-item a:hover {
+  text-decoration: none;
+}
+.nav-text {
+  margin-left: 1em;
+}
+.nav-icon a {
+  color: white;
+}
+</style>
+
---
+++ components/NavMenuItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav-menu-item" @click="$emit('menu-item-selected', link)">
+    <nuxt-link :to="link" v-if="!external">
+      <MenuItem :text="text" :icon="icon" :expanded="expanded"/>
+    </nuxt-link>
+    <a :href="link" target="_blank" v-else>
+      <MenuItem :text="text" :icon="icon" :expanded="expanded"/>
+    </a>
+  </div>
+</template>
+
+<script>
+import MenuItem from "~/components/MenuItem.vue";
+export default {
+  props: ["expanded", "text", "icon", "link", "external"],
+  components: {
+    MenuItem
+  }
+};
+</script>
+
+<style>
+.nav-menu-item a {
+  color: white;
+  text-decoration: none;
+}
+</style>
+
